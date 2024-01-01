66efe358d307fd03763eceb1066618cf35500f12
hasanhammoudah <https://github.com/hasanhammoudah/Dynamic-Chart-in-PHP>
2024-01-01 21:49:10 +0300

    NEW UPDATE

---
+++ lib/utils/app_styles.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+
+abstract class AppStyles {
+  static const TextStyle styleRegular16 = TextStyle(
+    fontFamily: "Montserrat",
+    fontSize: 16,
+    fontWeight: FontWeight.w400,
+    color: Color(0xff064060),
+  );
+
+  static const TextStyle styleMedium16 = TextStyle(
+    fontFamily: "Montserrat",
+    fontSize: 16,
+    fontWeight: FontWeight.w500,
+    color: Color(0xff064060),
+  );
+  static const TextStyle styleSemiBold16 = TextStyle(
+    fontFamily: "Montserrat",
+    fontSize: 16,
+    fontWeight: FontWeight.w600,
+    color: Color(0xff064060),
+  );
+  static const TextStyle styleSemiBold20 = TextStyle(
+    fontFamily: "Montserrat",
+    fontSize: 20,
+    fontWeight: FontWeight.w600,
+    color: Color(0xff064060),
+  );
+  static const TextStyle styleRegular12 = TextStyle(
+    fontFamily: "Montserrat",
+    fontSize: 12,
+    fontWeight: FontWeight.w400,
+    color: Color(0xffAAAAAA),
+  );
+  static const TextStyle styleSemiBold24 = TextStyle(
+    fontFamily: "Montserrat",
+    fontSize: 24,
+    fontWeight: FontWeight.w600,
+    color: Color(0xff4EB7F2),
+  );
+  static const TextStyle styleRegular14 = TextStyle(
+    fontFamily: "Montserrat",
+    fontSize: 14,
+    fontWeight: FontWeight.w400,
+    color: Color(0xffAAAAAA),
+  );
+  static const TextStyle styleSemiBold18 = TextStyle(
+    fontFamily: "Montserrat",
+    fontSize: 18,
+    fontWeight: FontWeight.w600,
+    color: Color(0xffffffff),
+  );
+
+  static const TextStyle styleBold16 = TextStyle(
+    fontFamily: "Montserrat",
+    fontSize: 16,
+    fontWeight: FontWeight.w700,
+    color: Color(0xff4EB7F2),
+  );
+
+  static const TextStyle styleMedium20 = TextStyle(
+    fontFamily: "Montserrat",
+    fontSize: 20,
+    fontWeight: FontWeight.w500,
+    color: Color(0xffffffff),
+  );
+}
