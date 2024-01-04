34f0467d89eeefac757129f44708118048f82cbf
hasan <hassun.h98@gmail.com>
2024-01-04 14:58:05 +0300

    NEW UPDATE

--- lib/views/widgets/quick_invoice.dart
+++ lib/views/widgets/quick_invoice.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:real_responsive_and_adaptive_design/views/widgets/custom_background_container.dart';
 import 'package:real_responsive_and_adaptive_design/views/widgets/lates_transaction.dart';
 import 'package:real_responsive_and_adaptive_design/views/widgets/quick_invoice_header.dart';
+import 'package:real_responsive_and_adaptive_design/views/widgets/title_text_field.dart';
 
 class QuickInvoice extends StatelessWidget {
   const QuickInvoice({super.key});
@@ -13,6 +14,10 @@ class QuickInvoice extends StatelessWidget {
         children: [
           QuickInvoiceHeader(),
           LatesTransaction(),
+          Divider(
+            height: 48,
+          ),
+          TitleTextField(title: 'Customer name', hint: 'Type customer name'),
         ],
       ),
     );
---
+++ lib/views/widgets/title_text_field.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:real_responsive_and_adaptive_design/utils/app_styles.dart';
+import 'package:real_responsive_and_adaptive_design/views/widgets/custom_text_field.dart';
+
+class TitleTextField extends StatelessWidget {
+  const TitleTextField({super.key, required this.title, required this.hint});
+  final String title, hint;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          title,
+          style: AppStyles.styleMedium16,
+        ),
+        const SizedBox(),
+        CustomTextField(
+          hint: hint,
+        ),
+      ],
+    );
+  }
+}
